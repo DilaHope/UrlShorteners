034491903f1279f4423eb505b49873bd9d0f5f73
DilaHope <ketinsuyann@gmail.com>
2024-06-27 17:24:24 +0200

    components_crée

--- app.vue
+++ app.vue
@@ -1,8 +1,3 @@
 <template>
-  <div class="text-4xl font-bold text-amber-500">
-   Hello Nuxt !😎
-   <p>Avez-vous un compte Github?
-   <Icon name="devicon:githubcodespaces"/>
-  </p>
-  </div>
+   <Hello/>
 </template>
---
+++ components/Hello.vue
@@ -0,0 +1,9 @@
+<template>
+<div>
+    <h2  class="text-4xl font-bold text-amber-500">Hello Nuxt !😎</h2>
+   
+   <p>Avez-vous un compte Github?
+   <Icon name="devicon:githubcodespaces"/>
+  </p>
+</div>
+</template>
