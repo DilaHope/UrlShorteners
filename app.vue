c0f229fcbd327eb70ed1b4231ca14e9aade39987
DilaHope <ketinsuyann@gmail.com>
2024-06-27 17:15:48 +0200

    incon_installée

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="text-4xl font-bold text-amber-500">
    Hello Nuxt !😎
+   <p>Avez-vous un compte Github?
+   <Icon name="devicon:githubcodespaces"/>
+  </p>
   </div>
 </template>
